7c2b41b67e4bc843ee99812c9e8383c88fd2d94a
Sambeet-KumarSahu <sambeetkuamr_sahu@crgl-thirdparty.com>
2021-09-28 19:14:57 +0530

    Pure react native setup for alakarte

---
+++ screens/CheckUserAccess/CheckUserAccess.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Register } from "./../Register/Register";
+
+export default class CheckUserAccess extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUserExist: false,
+    };
+  }
+  async componentDidMount() {
+    let user = await AsyncStorage.getItem("user");
+    if (user) {
+      this.setState({
+        isUserExist: true,
+      });
+    } else {
+      this.setState({
+        isUserExist: false,
+      });
+    }
+  }
+  render() {
+    return (
+      <>
+        {this.state.isUserExist ? (
+          <>{this.props.children}</>
+        ) : (
+          <>
+            <Register></Register>
+          </>
+        )}
+      </>
+    );
+  }
+}
